9609739fb61a42dfe56a83328e63a8642d2a4063
K <97007ken@gmail.com>
2023-02-25 23:31:15 +0800

    Update - finish ticket function

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support.ui import Select
+
 import utilities as utils
 
 config = utils.read_config()
@@ -18,7 +18,12 @@ options.add_argument("--disable-notifications")
 tw_hs_url = 'https://irs.thsrc.com.tw/IMINT/?locale=tw&_ga=2.61512805.853185086.1677227212-2135743068.1677227211'
 start_station = config.get('start_station')
 end_station = config.get('end_station')
-start_time = config.get('start_timed')
+start_time = config.get('start_time')
+regular_ticket = int(config.get('regular_ticket'))
+children_ticket = int(config.get('children_ticket'))
+disabled_ticket = int(config.get('disabled_ticket'))
+student_ticket = int(config.get('student_ticket'))
+senior_persons_ticket = int(config.get('senior_persons_ticket'))
 
 
 def driver_send_keys_xpath(locator, key):
@@ -50,20 +55,25 @@ def enter():
     except TimeoutException:
         pass
     print("進入系統")
-    driver_click_xpath(f'//*[@id="BookingS1Form"]/div[3]/div[1]/div/div[1]/div/select/option[{start_station + 1}]')
+    driver_click_xpath(f'//*[@id="BookingS1Form"]/div[3]/div[1]/div/div[1]/div/select/option[{start_station}]')
     print("已輸入起站")
-    driver_click_xpath(f'//*[@id="BookingS1Form"]/div[3]/div[1]/div/div[2]/div/select/option[{end_station + 1}]')
+    driver_click_xpath(f'//*[@id="BookingS1Form"]/div[3]/div[1]/div/div[2]/div/select/option[{end_station}]')
     print("已輸入迄站")
-    #    driver_click_xpath("//*[@id='mainBody']/div[9]/div[2]/div/div[2]/div[1]/span[29]")
-    #    print("已輸入日期")
-    #    driver_click_xpath("//*[@id='BookingS1Form']/div[3]/div[2]/div/div[2]/div[1]/select/option[2]")
     driver_click_xpath(f'//*[@id="BookingS1Form"]/div[3]/div[2]/div/div[2]/div[1]/select/option[{start_time}]')
     print("已輸入出發時間")
+    driver_click_xpath(f'//*[@id="BookingS1Form"]/div[4]/div[1]/div[1]/div/select/option[{regular_ticket + 1}]')
+    print("已輸入全票")
+    driver_click_xpath(f'//*[@id="BookingS1Form"]/div[4]/div[1]/div[2]/div/select/option[{children_ticket + 1}]')
+    print("已輸入孩童票")
+    driver_click_xpath(f'//*[@id="BookingS1Form"]/div[4]/div[1]/div[3]/div/select/option[{disabled_ticket + 1}]')
+    print("已輸入愛心票")
+    driver_click_xpath(f'//*[@id="BookingS1Form"]/div[4]/div[1]/div[4]/div/select/option[{student_ticket + 1}]')
+    print("已輸入大學生票")
+    driver_click_xpath(f'//*[@id="BookingS1Form"]/div[4]/div[1]/div[5]/div/select/option[{senior_persons_ticket + 1}]')
+    print("已輸入敬老票")
     time.sleep(1000)
 
 
 if __name__ == '__main__':
-    print(config.get('start_time'))
     enter()
     driver.quit()
-# //*[@id="BookingS1Form"]/div[3]/div[2]/div/div[2]/div[1]/select/option[2]
--- utilities.py
+++ utilities.py
@@ -20,18 +20,17 @@ start_station : '台北'
 end_station : '左營'
 # -------------------------------------------
 # 輸入出發時間 
-# | 00:00 00:30 01:00 01:30 02:00 02:30 03:00 03:30 04:00 04:30 |
-# | 05:00 05:30 06:00 06:30 07:00 07:30 08:00 08:30 09:00 09:30 |
-# | 10:00 10:30 11:00 11:30 12:00 12:30 13:00 13:30 14:00 14:30 |
-# | 15:00 15:30 16:00 16:30 17:00 17:30 18:00 18:30 19:00 19:30 |
-# | 20:00 20:30 21:00 21:30 22:00 22:30 23:00 23:30 24:00 24:30 |
+# | 00:00 00:30 05:00 05:30 06:00 06:30 07:00 07:30 08:00 08:30 |
+# | 09:00 09:30 10:00 10:30 11:00 11:30 12:00 12:30 13:00 13:30 |
+# | 14:00 14:30 15:00 15:30 16:00 16:30 17:00 17:30 18:00 18:30 |
+# | 19:00 19:30 20:00 20:30 21:00 21:30 22:00 22:30 23:00 23:30 |
 start_time : '00:00'
 # -------------------------------------------
 # 輸入票種及數量，沒有則填0
 regular_ticket : '0' #全票
 children_ticket : '0' #孩童票
 disabled_ticket : '0' #愛心票
-student_ticket : '0'  #大學生票
+student_ticket : '0' #大學生票
 senior_persons_ticket : '0' #敬老票
 """
                 )
@@ -55,10 +54,20 @@ def read_config():
             start_station = get_start_station(data['start_station'])
             end_station = get_end_station(data['end_station'])
             start_time = get_start_time(data['start_time'])
+            ticket_dict['regular_ticket'] = data['regular_ticket']
+            ticket_dict['children_ticket'] = data['children_ticket']
+            ticket_dict['disabled_ticket'] = data['disabled_ticket']
+            ticket_dict['student_ticket'] = data['student_ticket']
+            ticket_dict['senior_persons_ticket'] = data['senior_persons_ticket']
             config = {
                 'start_station': start_station,
                 'end_station': end_station,
-                'start_time': start_time
+                'start_time': start_time,
+                'regular_ticket': ticket_dict['regular_ticket'],
+                'children_ticket': ticket_dict['children_ticket'],
+                'disabled_ticket': ticket_dict['disabled_ticket'],
+                'student_ticket': ticket_dict['student_ticket'],
+                'senior_persons_ticket': ticket_dict['senior_persons_ticket']
             }
             return config
     except (KeyError, TypeError):
@@ -69,29 +78,34 @@ def read_config():
 
 
 time_list = [
-    '00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30', '04:00', '04:30',
-    '05:00', '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30',
-    '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30',
-    '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30',
-    '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30', '24:00'
+    '00:00', '00:30', '05:00', '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30',
+    '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30',
+    '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30',
+    '19:00', '19:30', '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30',
 ]
 station_list = [
     '南港', '台北', '板橋', '桃園', '新竹', '苗栗',
     '台中', '彰化', '雲林', '嘉義', '台南', '左營'
 ]
+ticket_dict = {
+    'regular_ticket': '0',
+    'children_ticket': '0',
+    'disabled_ticket': '0',
+    'student_ticket': '0',
+    'senior_persons_ticket': '0'
+}
 
 
 def get_start_station(start_station):
-    start_station = station_list.index(start_station) + 1
+    start_station = station_list.index(start_station) + 2
     return start_station
 
 
 def get_end_station(end_station):
-    end_station = station_list.index(end_station) + 1
+    end_station = station_list.index(end_station) + 2
     return end_station
 
 
 def get_start_time(start_time):
     start_time = time_list.index(start_time) + 2
     return start_time
-
