82459fa0554e465bf9a6b0bdc098ce871155adad
K <97007ken@gmail.com>
2023-02-25 18:34:10 +0800

    Optimize - use index instead of if else statement and finish the start_time function

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ options.add_argument("--disable-notifications")
 tw_hs_url = 'https://irs.thsrc.com.tw/IMINT/?locale=tw&_ga=2.61512805.853185086.1677227212-2135743068.1677227211'
 start_station = config.get('start_station')
 end_station = config.get('end_station')
+start_time = config.get('start_timed')
 
 
 def driver_send_keys_xpath(locator, key):
@@ -53,16 +54,16 @@ def enter():
     print("已輸入起站")
     driver_click_xpath(f'//*[@id="BookingS1Form"]/div[3]/div[1]/div/div[2]/div/select/option[{end_station + 1}]')
     print("已輸入迄站")
-#    driver_click_xpath("//*[@id='mainBody']/div[9]/div[2]/div/div[2]/div[1]/span[29]")
-#    print("已輸入日期")
-#    driver_click_xpath("//*[@id='BookingS1Form']/div[3]/div[2]/div/div[2]/div[1]/select/option[2]")
-    select = Select(driver.find_element_by_id("toTimeTable"))
-    select.select_by_visible_text(u"00:00")
+    #    driver_click_xpath("//*[@id='mainBody']/div[9]/div[2]/div/div[2]/div[1]/span[29]")
+    #    print("已輸入日期")
+    #    driver_click_xpath("//*[@id='BookingS1Form']/div[3]/div[2]/div/div[2]/div[1]/select/option[2]")
+    driver_click_xpath(f'//*[@id="BookingS1Form"]/div[3]/div[2]/div/div[2]/div[1]/select/option[{start_time}]')
     print("已輸入出發時間")
     time.sleep(1000)
 
 
 if __name__ == '__main__':
+    print(config.get('start_time'))
     enter()
     driver.quit()
-# //*[@id='BookingS1Form']/div[3]/div[2]/div/div[2]/div[1]/select/option[]
+# //*[@id="BookingS1Form"]/div[3]/div[2]/div/div[2]/div[1]/select/option[2]
--- utilities.py
+++ utilities.py
@@ -16,16 +16,23 @@ def config_file_generator():
 # 輸入起訖站
 # |  南港  台北  板橋  桃園  新竹  苗栗      |
 # |  台中  彰化  雲林  嘉義  台南  左營      |
-start_station : ''
-end_station : ''
+start_station : '台北'
+end_station : '左營'
+# -------------------------------------------
 # 輸入出發時間 
 # | 00:00 00:30 01:00 01:30 02:00 02:30 03:00 03:30 04:00 04:30 |
 # | 05:00 05:30 06:00 06:30 07:00 07:30 08:00 08:30 09:00 09:30 |
 # | 10:00 10:30 11:00 11:30 12:00 12:30 13:00 13:30 14:00 14:30 |
 # | 15:00 15:30 16:00 16:30 17:00 17:30 18:00 18:30 19:00 19:30 |
 # | 20:00 20:30 21:00 21:30 22:00 22:30 23:00 23:30 24:00 24:30 |
-
-start_time : ''
+start_time : '00:00'
+# -------------------------------------------
+# 輸入票種及數量，沒有則填0
+regular_ticket : '0' #全票
+children_ticket : '0' #孩童票
+disabled_ticket : '0' #愛心票
+student_ticket : '0'  #大學生票
+senior_persons_ticket : '0' #敬老票
 """
                 )
     sys.exit()
@@ -45,12 +52,13 @@ def read_config():
     try:
         with open('config.yml', 'r', encoding="utf8") as f:
             data = yaml.load(f, Loader=SafeLoader)
-            start = get_start_station(data['start_station'])
-            end = get_end_station(data['end_station'])
+            start_station = get_start_station(data['start_station'])
+            end_station = get_end_station(data['end_station'])
+            start_time = get_start_time(data['start_time'])
             config = {
-                'start_station': start,
-                'end_station': end,
-                'start_time' :data['start_time']
+                'start_station': start_station,
+                'end_station': end_station,
+                'start_time': start_time
             }
             return config
     except (KeyError, TypeError):
@@ -60,96 +68,30 @@ def read_config():
         sys.exit()
 
 
-def get_start_station(start):
-    if start == "南港":
-        start = 1
-    elif start == "台北":
-        start = 2
-    elif start == "板橋":
-        start = 3
-    elif start == "桃園":
-        start = 4
-    elif start == "新竹":
-        start = 5
-    elif start == "苗栗":
-        start = 6
-    elif start == "台中":
-        start = 7
-    elif start == "彰化":
-        start = 8
-    elif start == "雲林":
-        start = 9
-    elif start == "嘉義":
-        start = 10
-    elif start == "台南":
-        start = 11
-    elif start == "左營":
-        start = 12
-    return start
+time_list = [
+    '00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30', '04:00', '04:30',
+    '05:00', '05:30', '06:00', '06:30', '07:00', '07:30', '08:00', '08:30', '09:00', '09:30',
+    '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30',
+    '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30',
+    '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30', '24:00'
+]
+station_list = [
+    '南港', '台北', '板橋', '桃園', '新竹', '苗栗',
+    '台中', '彰化', '雲林', '嘉義', '台南', '左營'
+]
 
 
-def get_end_station(end):
-    if end == "南港":
-        end = 1
-    elif end == "台北":
-        end = 2
-    elif end == "板橋":
-        end = 3
-    elif end == "桃園":
-        end = 4
-    elif end == "新竹":
-        end = 5
-    elif end == "苗栗":
-        end = 6
-    elif end == "台中":
-        end = 7
-    elif end == "彰化":
-        end = 8
-    elif end == "雲林":
-        end = 9
-    elif end == "嘉義":
-        end = 10
-    elif end == "台南":
-        end = 11
-    elif end == "左營":
-        end = 12
-    return end
+def get_start_station(start_station):
+    start_station = station_list.index(start_station) + 1
+    return start_station
 
 
-#time_dict = {
-#    '00:00': 2, '00:30': 3, '01:00': 4, '01:30': 5, '02:00': 6, '02:30': 7, '03:00': 8, '03:30': 9, '04:00': 10,
-#
-#    '04:30': 11,
-#    '05:00': 12, '05:30': 13, '06:00': 14, '06:30': 15, '07:00': 16, '07:30': 17, '08:00': 18, '08:30': 19, '09:00': 20,
-#    '09:30': 21,
-#}
+def get_end_station(end_station):
+    end_station = station_list.index(end_station) + 1
+    return end_station
 
 
-def get_start_time(time):
-    if time == "00:00":
-        time = 2
-    elif time == "00:30":
-        time = 3
-    elif time == "01:00":
-        time = 4
-    elif time == "01:30":
-        time = 5
-    elif time == "02:00":
-        time = 6
-    elif time == "02:30":
-        time = 7
-    elif time == "03:30":
-        time = 8
-    elif time == "04:00":
-        time = 9
-    elif time == "05:00":
-        time = 10
-    elif time == "05:30":
-        time = 11
-    elif time == "06:00":
-        time = 12
-    elif time == "06:30":
-        time = 13
-    elif time == "07:00":
-        time = 14
-    return time
+def get_start_time(start_time):
+    start_time = time_list.index(start_time) + 2
+    return start_time
+
